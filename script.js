dda74e24080fc3b0894eb6064d7f2791eaf986fd
taigataiga02244211-boop <taigataiga02244211@gmail.com>
2026-03-19 22:27:04 +0900

    修正

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ function toggleTimer() {
         document.getElementById("timer").textContent = "00:00:00";
 
         updateRecord(studyhours, studyMinutes, studySeconds);
+        sendStudyData();
     }
 }
 
@@ -70,3 +71,65 @@ function updateRecord(studyhours, studyMinutes, studySeconds){
     
     document.getElementById("history").appendChild(record);
 }
+
+//保存
+function saveName(){
+    const name = document.getElementById("username").value;
+    localStorage.setItem("username", name);
+}
+
+//取り出す
+const username = localStorage.getItem("username");
+document.getElementById("username").value = localStorage.getItem("username");
+
+//exec 書き込み専用 CSV 読み込み専用
+//書き込み
+function sendStudyData(){
+    const username = localStorage.getItem("username");
+    const subject = document.getElementById("startContent").value;
+    const totaltime = totaltime;
+
+    fetch("https://script.google.com/macros/s/AKfycbwLkvn-UJao1oTJY8nleqPTqbPk0KFXva7GaYQ6nZ3rcRyuljj-5iaHSX1CIWU0r3iT/exec", {
+        method: "POST",
+        body: JSON.stringify({
+            name: username,
+            subject: subject,
+            time:totaltime
+        })    
+    })
+    .then(response => console.log("成功"))
+    .catch(error => console.log(error));
+}
+
+//読み込み
+fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQwQtcZ2JlqvdupX6iiZGwAsx0BTusii3nIzZA6hx4u_VwsBZlVfrmzmJz1TOTlHzH9IM6H0byYqVIx/pub?output=csv")
+  .then(response => response.text())
+  .then(data => {
+      const lines = data.split("\n");
+      const totals = {};
+
+      lines.forEach(line => {
+          const cols = line.split(",");
+          const name = cols[0];
+          const time = Number(cols[2].trim());
+          
+          if(!name || isNaN(time)) return;
+
+          if(totals[name]){
+              totals[name] += time;
+          } else {
+              totals[name] = time;
+          }
+      });
+
+      const ranking = Object.entries(totals)
+          .sort((a,b)=> b[1]-a[1]);
+
+      let text = "";
+
+      ranking.forEach((item,index)=>{
+          text += `${index+1}位 ${item[0]} ${item[1]}分<br>`;
+      });
+
+      document.getElementById("ranking").innerHTML = text;
+  });
